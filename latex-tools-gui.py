cdb08c8f57c5f46660951f8ec0d04c8793a8dfae
Prof. Dr. Claudius Stern <stern@biozoom.net>
2022-11-12 09:18:14 +0100

    Hello-World GUI mit Klasse

---
+++ latex-tools-gui.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class LatexToolsGui:
+    root: tk.Tk
+
+    def __init__(self, root_tk):
+        '''
+        Constructor. Initializes the GUI class.
+        '''
+        self.root = root_tk  # Object variable
+        self.init_gui()
+
+    def get_root(self):
+        return self.root
+
+    def init_gui(self):
+        frame = ttk.Frame(self.root, padding=10)
+        frame.grid()
+        ttk.Label(frame,
+                  text='Hello World!').grid(row=0, column=0)
+        ttk.Button(frame,
+                   text='Quit',
+                   command=self.root.destroy).grid(row=1, column=0)
+
+
+if __name__ == '__main__':
+    gui = LatexToolsGui(tk.Tk())
+    gui.get_root().mainloop()
