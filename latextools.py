1a597957335fdab464b40b96b939ad094a2157a3
Prof. Dr. Claudius Stern <stern@biozoom.net>
2022-11-30 19:57:58 +0100

    Parameter "first_row_changed" und "last_row_changed" hinzugefügt

--- latextools.py
+++ latextools.py
@@ -1,20 +1,34 @@
-def make_latex_table(tabelle, booktab=False):
+def make_latex_table(tabelle,
+                     booktab=False,
+                     first_row_changed=False,
+                     last_row_changed=False):
     '''
 
     :param booktab: If true, the table will get booktab-style lines.
+    :param first_row_changed: If true, a line will be inserted after first line
+    :param last_row_changed: If true, a line will be inserted before last line
     :param tabelle: A list of lists representing the table content.
     :return:
     '''
 
     table_width = 0
+    table_length = len(tabelle)
     row_content = ''
     if booktab:
         row_content += '\\toprule\n'
-    for row in tabelle:
+    for i, row in enumerate(tabelle):
         cur_len = len(row)
         if cur_len > table_width:
             table_width = cur_len
         row_content += ' & '.join(row)+'\\\\\n'
+        if first_row_changed and i == 0:
+            if booktab:
+                row_content += '\\midrule\\\\\n'
+            else:
+                row_content += '\\hrule\\\\\n'
+        elif last_row_changed and i == table_length - 2:
+            # Second last row, so add rule
+            row_content += '\\' + ('mid' if booktab else 'h') + 'rule\\\\\n'
     if booktab:
         row_content += r'\bottomrule'
     latex_code = r'''
@@ -32,5 +46,5 @@ if __name__ == '__main__':
     with open('ExcelTest1.CSV') as f:
         csv_reader = csv.reader(f, delimiter=';')
         tabelle = [line for line in csv_reader]
-    latex_tabelle = make_latex_table(tabelle)
+    latex_tabelle = make_latex_table(tabelle, booktab=True, first_row_changed=True, last_row_changed=True)
     print(latex_tabelle)
